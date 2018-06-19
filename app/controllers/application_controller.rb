655fefde235597a2c8f1d0bbbb3337529ffa7092
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-06-19 12:22:13 -0400

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < Sinatra::Base
+
+  set :views, Proc.new { File.join(root, "../views/") }
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+end
